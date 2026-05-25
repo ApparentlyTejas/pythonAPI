7c363ae934fbc9fb81ccbfb50a8bdc4e66bb7b74
ApparentlyTejas <tejas.malhotra.14@gmail.com>
2026-05-26 01:22:03 +0200

    .

--- app.py
+++ app.py
@@ -28,6 +28,30 @@ with app.app_context():
 def home():
     return jsonify({"message": "Welcome to Travel API"})
 
+@app.route("/destinations", methods=["GET"])
+def get_destinations():
+    destinations = Destination.query.all()
+
+    return jsonify([destination.to_dict()]for destination in destinations)
+
+@app.route("/destinations/<int:destination_id>", methods=("GET"))
+def get_destination(destination_id):
+    destination = Destination.query.get(destination_id)
+    if destination:
+        return jsonify(destination.to_dict())
+    else:
+        return jsonify({"404 ERROR"})
+
+@app.route("/destinations", methods=["POST"])
+def add_destination():
+    data = request.get_json()
+
+    new_destination = Destination(destination=data["destination"], country=data["country"], rating=data["rating"])
+
+    db.session.add(new_destination)
+    db.session.commit()
+
+    return jsonify(new_destination.to_dict()), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
